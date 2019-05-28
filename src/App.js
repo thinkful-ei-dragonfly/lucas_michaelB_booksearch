7acd274aab5af210c52ac13e4ba4deb087334abc
Michael Bravo <michael.bravo777@gmail.com>
2019-05-28 12:44:52 -0400

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import ResultsList from './components/ResultsList/ResultsList'
@@ -13,21 +13,65 @@ import SearchFilters from './components/SearchFilters/SearchFilters'
 // -> list of bookResult components
 // -> clickHandler to expand and show description + price and whatever
 //
-function App() {
-  return (
-    <main className="App" role='main'>
-      <Header role='banner'></Header>
-      <div className='search_filter'>
-        <SearchForm></SearchForm>
-        <SearchFilters></SearchFilters>
-      </div>
-      <section className='resultsContainer'>
-        <ResultsList/>
-      </section>
+export default class App extends Component {
+  state = {
+    books: [],
+    loading: false,
+    error: null
+  };
+
+  fetchBooks = (e) => {
+    e.preventDefault();
+    this.setState({loading: true});
+    let newBooks = [];
+    const searchQuery = e.target.search.value;
+    fetch('https://www.googleapis.com/books/v1/volumes?key=AIzaSyB23nntppFskdPBsHXbL7gaFzNKOSX_FtM&q=' + searchQuery)
+    .then(res => res.json())
+    .then(books => {
+      newBooks = books.items.map(book => {
+        return {
+          id: book.id,
+          name: book.volumeInfo.title,
+          authors: book.volumeInfo.authors,
+          description: book.volumeInfo.description,
+          printType: book.volumeInfo.printType,
+          price: book.saleInfo.listPrice,
+          isEbook: book.saleInfo.isEbook,
+          thumbnail: book.volumeInfo.imageLinks.thumbnail
+        }
+      })
+      return newBooks;
+    })
+    .then(newBooks => this.setState({
+      books: newBooks,
+      loading: false
+    }))
+  }
+  // price: `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}`,
+
+  render(){
+    let loading = '';
+    if (this.state.loading) {
+      loading = <div>Loading...</div>;
+    }
+    return (
+      <main className="App" role='main'>
+        <Header role='banner'></Header>
+        <div className='search_filter'>
+          <SearchForm
+            fetchBooks={this.fetchBooks} >
+
+          </SearchForm>
+          <SearchFilters></SearchFilters>
+        </div>
+        <section className='resultsContainer'>
+          {loading}
+          <ResultsList/>
+        </section>
 
 
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
-export default App;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default class SearchForm extends React.Component{
-  render(){
-    return (
-      <form id='bookSearch'>
-        <label htmlFor='search'>Search:</label>
-        <input type='text' id='search' name='search' placeholder='Search for books..'></input>
-        <button type='submit'>Search</button>
-      </form>
-    )
-  }
+function SearchForm (props) {
+  return (
+    <form id='bookSearch' onSubmit={props.fetchBooks}>
+      <label htmlFor='search'>Search:</label>
+      <input type='text' id='search' name='search' placeholder='Search for books..'></input>
+      <button type='submit'>Search</button>
+    </form>
+  )
 }
+
+export default SearchForm;
